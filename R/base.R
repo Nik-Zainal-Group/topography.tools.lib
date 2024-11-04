58256708d3d95a5d8368b5e7a7f9979f27a043ce
Andrea Degasperi <ad923@cam.ac.uk>
2024-11-04 17:32:06 +0000

    base functions for sorting positions and bed tables. Function for checking bed regions overlaps

--- R/base.R
+++ R/base.R
@@ -4,7 +4,7 @@ NULL
 
 #' Sort chromosomes
 #'
-#' Order a list of chromosome names.
+#' Sort a list of chromosome names.
 #' 
 #' @param chroms characters vector with chromosome names, chr prefix is optional, so, for example, "1" and "chr1" are both acceptable
 #' @param decreasing if TRUE chromosomes are sorted in decreasing order from higher chromosome number
@@ -28,3 +28,98 @@ sortChroms <- function(chroms,
   return(txtchroms[order(n,decreasing = decreasing)])
 }
 
+#' Sort positions
+#'
+#' Sort a list of positions. Positions may be genomic coordinates of small variants or polymorphisms
+#' 
+#' @param positions data frame with at least two required columns: chr, position
+#' @return data frame of ordered positions
+#' @export
+sortPositions <- function(positions){
+  if(!all(colnames(positions) %in% c("chr","position"))){
+    misscol <- setdiff(c("chr","position"),colnames(positions))
+    message("[error sortPositions] missing columns: ",paste(misscol,collapse = ", "))
+    return(NULL)
+  }
+  if(nrow(positions)==0){
+    message("[warning sortPositions] no positions to sort")
+    return(positions)
+  }
+  positions$chr <- as.character(positions$chr)
+  sortedPositions <- NULL
+  chroms <- sortChroms(unique(positions$chr))
+  for (chrom in chroms){
+    # chrom <- chroms[1]
+    tmpTable <- positions[positions$chr==chrom,,drop=F]
+    tmpTable <- tmpTable[order(tmpTable$position),,drop=F]
+    sortedPositions <- rbind(sortedPositions,tmpTable)
+  }
+  return(sortedPositions)
+}
+
+#' Sort bed
+#'
+#' Sort a list of bed regions, according to the chromosome (chr column) and start
+#' position (start column). If a bed region has end < start then the start and end
+#' are swapped so that start <= end in the sorted bed table.
+#' 
+#' @param bed_table data frame with at least three required columns: chr, start, end
+#' @return data frame of ordered bed regions
+#' @export
+sortBed <- function(bed_table){
+  if(!all(colnames(bed_table) %in% c("chr","start","end"))){
+    misscol <- setdiff(c("chr","start","end"),colnames(bed_table))
+    message("[error sortBed] missing columns: ",paste(misscol,collapse = ", "))
+    return(NULL)
+  }
+  if(nrow(bed_table)==0){
+    message("[warning sortBed] no bed regions to sort")
+    return(bed_table)
+  }
+  bed_table$chr <- as.character(bed_table$chr)
+  sortedBed <- NULL
+  chroms <- sortChroms(unique(bed_table$chr))
+  for (chrom in chroms){
+    # chrom <- chroms[1]
+    tmpTable <- bed_table[bed_table$chr==chrom,,drop=F]
+    # check if you need to reorder some start and end
+    selinvert <- tmpTable$end < tmpTable$start
+    if(sum(selinvert)>0){
+      message("[warning sortBed] instances of start > end in ",sum(selinvert)," regions in chromosome ",chrom,
+              ". Fixed by swapping start and end so that start comes first.")
+      selstarts <- tmpTable$start[selinvert]
+      tmpTable$start[selinvert] <- tmpTable$end[selinvert]
+      tmpTable$end[selinvert] <- selstarts
+    } 
+    tmpTable <- tmpTable[order(tmpTable$start),,drop=F]
+    sortedBed <- rbind(sortedBed,tmpTable)
+  }
+  return(sortedBed)
+}
+
+#' Check existence of bed regions overlaps
+#'
+#' Return NULL if all the bed regions in bed_table are disjoint, otherwise return
+#' the list of chromosomes where the region overlaps occur.
+#' 
+#' @param bed_table data frame with at least three required columns: chr, start, end
+#' @return NULL if no overlaps are found, or a list of chromosome names where overlaps were found
+#' @export
+checkBedRegionsOverlap <- function(bed_table){
+  bed_table <- sortBed(bed_table)
+  chroms <- unique(bed_table$chr)
+  
+  overlapChroms <- NULL
+  for (chrom in chroms) {
+    # chrom <- chroms[1]
+    tmpTable <- bed_table[bed_table$chr==chrom,,drop=F]
+    if(nrow(tmpTable)>1){
+      existsOverlap <- any(tmpTable$end[1:(nrow(tmpTable)-1)] >= tmpTable$start[2:nrow(tmpTable)])
+      if(existsOverlap) overlapChroms <- c(overlapChroms,chrom)
+    }
+  }
+  if(!is.null(overlapChroms)){
+    message("[info checkBedRegionsOverlap] Regions overlap identified in chromosomes ",paste(overlapChroms,collapse = ", "))
+  }
+  return(overlapChroms)
+}
--- tests/testthat/test-base.R
+++ tests/testthat/test-base.R
@@ -1,12 +1,48 @@
 context("testing base functions")
 
-test_that("test sort chromosomes", {
+test_that("test sortChroms", {
   
   chroms <- c("12","7","Y","X")
   expect_sorted <- c("7","12","X","Y")
   
   chroms_sorted <- sortChroms(chroms)
   
-  expect_equal( chroms_sorted, expect_sorted )
+  expect_equal(chroms_sorted,expect_sorted)
+  
+})
+
+test_that("test sortPositions", {
+  
+  positions <- data.frame(chr=c("2","2","1","1"),position=c(2,1,2,1))
+  expect_sorted <- positions[c(4,3,2,1),]
+  
+  positions_sorted <- sortPositions(positions)
+  
+  expect_equal(positions_sorted,expect_sorted)
+  
+})
+
+test_that("test sortBed", {
+  
+  bed_table <- data.frame(chr=c("2","2","1","1"),start=c(3,2,2,1),end=c(1,4,3,2))
+  expect_sorted <- bed_table[c(4,3,1,2),]
+  expect_sorted[3,c("start","end")] <- c(1,3)
+  
+  bed_table_sorted <- sortBed(bed_table)
+  
+  expect_equal(bed_table_sorted,expect_sorted)
+  
+})
+
+test_that("test checkBedRegionsOverlap", {
+  
+  bed_table1 <- data.frame(chr=c("2","2","1","1"),start=c(3,2,2,1),end=c(1,4,3,2))
+  bed_table2 <- data.frame(chr=c("2","2","1","1"),start=c(3,5,2,6),end=c(1,6,3,8))
+  
+  bed_table_res1 <- checkBedRegionsOverlap(bed_table1)
+  bed_table_res2 <- checkBedRegionsOverlap(bed_table2)
+  
+  expect_equal(bed_table_res1,c("1","2"))
+  expect_equal(bed_table_res2,NULL)
   
 })
